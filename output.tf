b0b5a9f95f9acd29657c7e4a31f0fe8a7a3cb853
kada2004 <dankalenga3@gmail,com>
2025-07-13 21:25:34 +0200

    Test CI/CD WORKFLOW

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "The name of the resource group"
+}
+
+output "storage_account_name" {
+  value       = azurerm_storage_account.stg_account.name
+  description = "The name of the storage account"
+}
+
+output "container_name" {
+  value       = azurerm_storage_container.state_file.name
+  description = "The name of the blob container"
+}
+
