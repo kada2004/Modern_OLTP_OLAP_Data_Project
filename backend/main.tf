c5e0300a0434e8393f1d078f053e9699f93eb391
kada2004 <dankalenga3@gmail,com>
2025-07-15 20:39:17 +0200

    fix_deploy_pipeline

--- backend/main.tf
+++ backend/main.tf
@@ -63,6 +63,7 @@ resource "azurerm_key_vault" "kv" {
   soft_delete_retention_days = 7
 }
 
+#https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault_access_policy
 resource "azurerm_key_vault_access_policy" "terraform" {
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
