312e192443720210e61ce014732b7a00e487e822
kada2004 <dankalenga3@gmail,com>
2025-07-15 17:14:22 +0200

    SP connection

--- backend/main.tf
+++ backend/main.tf
@@ -65,8 +65,8 @@ resource "azurerm_key_vault" "kv" {
 
 resource "azurerm_key_vault_access_policy" "terraform" {
   key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_subscription.current.tenant_id
-  object_id    = var.terraform_sp_object_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
 
   secret_permissions = ["Get", "Set", "List"]
 }
--- backend/variables.tf
+++ backend/variables.tf
@@ -1,8 +1,3 @@
-variable "terraform_sp_object_id" {
-  description = "Object ID of the service principal used by Terraform"
-  type        = string
-  sensitive   = true
-}
 
 variable "synapse_sql_password" {
   description = "Password for Synapse SQL admin"
